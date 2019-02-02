65ecdd0b8757999f6a6b124fdcf3048a17ca0548
bartmika <bart@mikasoftware.com>
2019-02-02 15:43:39 -0500

    Latest progress with developing the store.

--- internal/serializer/store.go
+++ internal/serializer/store.go
@@ -10,6 +10,96 @@ import (
     // "github.com/luchacomics/comicscantina-go/internal/model_manager"
 )
 
+
+//----------------------------------------------------------------------------//
+//                                  CREATE                                    //
+//----------------------------------------------------------------------------//
+
+
+// StoreDetailRequest is the request payload for Store data model.
+type StoreDetailRequest struct {
+    Name                string `json:"name"; form:"name";`
+    Description         string `json:"description,omitempty"; form:"description";`
+    // OrganizationID      uint64 `json:"organization_id,omitempty"`
+    Email               string `json:"email"; form:"email";`
+    StreetAddress       string `json:"street_address"; form:"street_address";`
+    StreetAddressExtra  string `json:"street_address_extra"; form:"street_address_extra";`
+    City                string `json:"city"; form:"city";`
+    Province            string `json:"province"; form:"province";`
+    Country             string `json:"country"; form:"country";`
+    Currency            string `json:"currency"; form:"currency";`
+    Language            string `json:"language"; form:"language";`
+    Website             string `json:"website"; form:"website";`
+    Phone               string `json:"phone"; form:"phone";`
+    Fax                 string `json:"fax"; form:"fax";`
+}
+
+// Function will validate the input payload.
+func (data *StoreDetailRequest) Bind(r *http.Request) error {
+    if data.Name == "" {
+        return errors.New("Missing name.")
+    }
+    // if data.OrganizationID == 0 {
+    //     return errors.New("Missing organization_id.")
+    // }
+    // TODO: Check to see if this organization exists?
+    // TODO: Check to see if the authenticated user belongs to this organization.
+    if data.Email == "" {
+        return errors.New("Missing email.")
+    }
+    if data.StreetAddress == "" {
+        return errors.New("Missing street address.")
+    }
+    if data.City == "" {
+        return errors.New("Missing city.")
+    }
+    if data.Province == "" {
+        return errors.New("Missing province.")
+    }
+    if data.Country == "" {
+        return errors.New("Missing country.")
+    }
+
+    // Return with no errors.
+	return nil
+}
+
+// Function will create Store data model from the input payload.
+func (data *StoreDetailRequest) Save(ctx context.Context) (*model.Store, error) {
+    // The model we will be creating.
+    var store model.Store
+
+    // Create our `User` object in our database.
+    store = model.Store {
+        Name:               data.Name,
+        Description:        data.Description,
+        Email:              data.Email,
+        // OrganizationID:     data.OrganizationID,
+        // CreatedAt:    time.Now(),
+        // UpdatedAt:    time.Now(),
+        StreetAddress:      data.StreetAddress,
+        StreetAddressExtra: data.StreetAddressExtra,
+        City:               data.City,
+        Province:           data.Province,
+        Country:            data.Country,
+        Currency:           data.Currency,
+        Language:           data.Language,
+        Website:            data.Website,
+        Phone:              data.Phone,
+        Fax:                data.Fax,
+    }
+
+    // Get our database connection.
+    dao := database.Instance()
+    db := dao.GetORM()
+
+    // Create our object in the database.
+    db.Create(&store)
+
+    return &store, nil
+}
+
+
 //----------------------------------------------------------------------------//
 //                                  LIST                                      //
 //----------------------------------------------------------------------------//
@@ -48,98 +138,6 @@ func NewStoreListResponse(stores []model.Store) []render.Renderer {
 		list = append(list, NewStoreListItemResponse(&store))
 	}
 	return list
-}
-
-
-//----------------------------------------------------------------------------//
-//                                  CREATE                                    //
-//----------------------------------------------------------------------------//
-
-
-// StoreDetailRequest is the request payload for Store data model.
-type StoreDetailRequest struct {
-    Name                string `json:"name"; form:"name";`
-    Description         string `json:"description,omitempty"; form:"description";`
-    OrganizationID      uint64 `json:"organization_id,omitempty" form:"int"`
-    Email               string `json:"email"; form:"email";`
-    StreetAddress       string `json:"street_address"; form:"street_address";`
-    StreetAddressExtra  string `json:"street_address_extra"; form:"street_address_extra";`
-    City                string `json:"city"; form:"city";`
-    Province            string `json:"province"; form:"province";`
-    Country             string `json:"country"; form:"country";`
-    Currency            string `json:"currency"; form:"currency";`
-    Language            string `json:"language"; form:"language";`
-    Website             string `json:"website"; form:"website";`
-    Phone               string `json:"phone"; form:"phone";`
-    Fax                 string `json:"fax"; form:"fax";`
-}
-
-// Function will validate the input payload.
-func (data *StoreDetailRequest) Bind(r *http.Request) error {
-    if data.Name == "" {
-        return errors.New("Missing name.")
-    }
-    if data.OrganizationID == 0 {
-        return errors.New("Missing organization_id.")
-    }
-    // TODO: Check to see if this organization exists?
-    // TODO: Check to see if the authenticated user belongs to this organization.
-    if data.Email == "" {
-        return errors.New("Missing email.")
-    }
-    if data.StreetAddress == "" {
-        return errors.New("Missing street address.")
-    }
-    if data.City == "" {
-        return errors.New("Missing city.")
-    }
-    if data.Province == "" {
-        return errors.New("Missing province.")
-    }
-    if data.Country == "" {
-        return errors.New("Missing country.")
-    }
-
-    // Return with no errors.
-	return nil
-}
-
-// Function will create Store data model from the input payload.
-func (data *StoreDetailRequest) Save(ctx context.Context) (*model.Store, error) {
-    // Extract the current user from the request context.
-    user := ctx.Value("user").(*model.User)
-
-    // The model we will be creating.
-    var store model.Store
-
-    // Create our `User` object in our database.
-    store = model.Store {
-        Name:               data.Name,
-        Description:        data.Description,
-        Email:              data.Email,
-        OrganizationID:     data.OrganizationID,
-        // CreatedAt:    time.Now(),
-        // UpdatedAt:    time.Now(),
-        StreetAddress:      data.StreetAddress,
-        StreetAddressExtra: data.StreetAddressExtra,
-        City:               data.City,
-        Province:           data.Province,
-        Country:            data.Country,
-        Currency:           data.Currency,
-        Language:           data.Language,
-        Website:            data.Website,
-        Phone:              data.Phone,
-        Fax:                data.Fax,
-    }
-
-    // Get our database connection.
-    dao := database.Instance()
-    db := dao.GetORM()
-
-    // Create our object in the database.
-    db.Create(&store)
-
-    return &store, nil
 }
 
 
