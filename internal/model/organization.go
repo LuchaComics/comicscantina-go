cb87eb21f51d69f4967831c20ae21e7c4db878ff
bartmika <bart@mikasoftware.com>
2019-02-01 21:00:43 -0500

    Added more fields.

--- internal/model/organization.go
+++ internal/model/organization.go
@@ -10,48 +10,18 @@ type Organization struct {
     Name                string `gorm:"not null; unique; size:255;"`
     Description         string `gorm:"size:1023;"`
     Email               string `gorm:"not null; unique; size:255;"`
-    // Status              uint8
+    Status              uint8 `gorm:"default: 1;"`
     OwnerID             uint64 `gorm:"index"`
-    // StreetAdddress      string `gorm:"size:127;"`
-    // StreetAdddressExtra string `gorm:"size:127;"`
-    // City                string `gorm:"size:127;"`
-    // Province            string `gorm:"size:127;"`
-    // Country             string `gorm:"size:127;"`
-    // Currency string `gorm:"type:varchar(127)”`
-    // Language string `gorm:"type:varchar(2)”`
-    // Website string `gorm:"type:varchar(255)”`
-    // Phone string `gorm:"type:varchar(10)”`
-    // Fax string `gorm:"type:varchar(10)”`
-
-    // CreatedAt time.Time `gorm:"DEFAULT:current_timestamp"`
-    // UpdatedAt time.Time `gorm:"DEFAULT:current_timestamp"`
-    // DeletedAt time.Time
-
-    // # Social Media
-    // twitter = models.CharField(max_length=15, null=True, blank=True)
-    // facebook_url = models.URLField(null=True, blank=True)
-    // instagram_url = models.URLField(null=True, blank=True)
-    // linkedin_url = models.URLField(null=True, blank=True)
-    // github_url = models.URLField(null=True, blank=True)
-    // google_url = models.URLField(null=True, blank=True)
-    // youtube_url = models.URLField(null=True, blank=True)
-    // flickr_url = models.URLField(null=True, blank=True)
-    //
-    // # Payment Processing Accounts
-    // paypal_email = models.EmailField()
-    //
-    // # Look and Feel
-    // header = models.ForeignKey(ImageUpload, null=True, blank=True, related_name='org_header',)
-    // logo = models.ForeignKey(ImageUpload, null=True, blank=True, related_name='org_logo',)
-    // style = models.CharField(
-    //     max_length=31,
-    //     choices=constants.TSHOP_THEME_OPTIONS,
-    //     default='ecantina-style-5.css',
-    // )
-    //
-    // # Users
-    // administrator = models.ForeignKey(User, null=True,)
-    // customers = models.ManyToManyField(Customer, blank=True)
+    StreetAdddress      string `gorm:"size:127;"`
+    StreetAdddressExtra string `gorm:"size:127;"`
+    City                string `gorm:"size:127;"`
+    Province            string `gorm:"size:127;"`
+    Country             string `gorm:"size:127;"`
+    Currency            string  `gorm:"type:varchar(3)"`
+    Language            string `gorm:"size:2;"`
+    Website             string `gorm:"size:127;"`
+    Phone               string `gorm:"size:10;"`
+    Fax                 string `gorm:"size:10;"`
 }
 
 // Give custom table name in our database.
